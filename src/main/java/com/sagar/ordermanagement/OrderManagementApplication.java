f0a58f695c93499c9b9bb71303e4bc8fd7a02afd
Vidyasagar Thangellapally <vthangellapally@nisum.com>
2020-01-23 18:47:14 +0530

    changed base package

--- src/main/java/com/sagar/ordermanagement/OrderManagementApplication.java
+++ src/main/java/com/sagar/ordermanagement/OrderManagementApplication.java
@@ -1,4 +1,4 @@
-package com.sagar.poc1;
+package com.sagar.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
